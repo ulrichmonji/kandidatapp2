1a2ac14d9b2a0c79c54274ae7a7c2f6ba523a4ea
ngouana001 <114586861+ngouana001@users.noreply.github.com>
2023-03-12 20:30:08 +0100

    Update views.py

--- kandidat/views.py
+++ kandidat/views.py
@@ -50,7 +50,6 @@ def kandidaten_detail(request, pk):
     if request.method == 'GET':
         kandidat_serializer = KandidatSerializer(kandidat)
         return JsonResponse(kandidat_serializer.data)
-
     elif request.method == 'PUT':
         kandidat_serializer = KandidatSerializer(kandidat,data=request.data)
         if kandidat_serializer.is_valid():
@@ -69,7 +68,6 @@ def kandidaten_list_erwachsene(request):
     if request.method == 'GET':
         kandidat_serializer = KandidatSerializer(kandidat, many=True)
         return JsonResponse(kandidat_serializer.data, safe=False)
-
 def home(request):
 
     return render(request,'kandidat/layout.html')
